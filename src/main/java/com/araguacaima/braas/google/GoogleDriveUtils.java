faea172615e8a9af1db9d77aa294a808d0297968
Alejandro Manuel Méndez Araguacaima <araguacaima@gmail.com>
2019-04-23 22:57:54 -0500

    Little fixes

--- src/main/java/com/araguacaima/braas/google/GoogleDriveUtils.java
+++ src/main/java/com/araguacaima/braas/google/GoogleDriveUtils.java
@@ -2,16 +2,11 @@ package com.araguacaima.braas.google;
 
 import com.araguacaima.braas.Constants;
 import com.google.api.client.auth.oauth2.Credential;
-import com.google.api.client.extensions.java6.auth.oauth2.AuthorizationCodeInstalledApp;
-import com.google.api.client.extensions.jetty.auth.oauth2.LocalServerReceiver;
-import com.google.api.client.googleapis.auth.oauth2.GoogleAuthorizationCodeFlow;
-import com.google.api.client.googleapis.auth.oauth2.GoogleClientSecrets;
 import com.google.api.client.googleapis.auth.oauth2.GoogleCredential;
 import com.google.api.client.googleapis.javanet.GoogleNetHttpTransport;
 import com.google.api.client.http.javanet.NetHttpTransport;
 import com.google.api.client.json.JsonFactory;
 import com.google.api.client.json.jackson2.JacksonFactory;
-import com.google.api.client.util.store.FileDataStoreFactory;
 import com.google.api.services.drive.Drive;
 import com.google.api.services.drive.DriveScopes;
 import org.slf4j.Logger;
@@ -20,7 +15,6 @@ import org.slf4j.LoggerFactory;
 import java.io.ByteArrayOutputStream;
 import java.io.IOException;
 import java.io.InputStream;
-import java.io.InputStreamReader;
 import java.security.GeneralSecurityException;
 import java.util.Collections;
 import java.util.List;
@@ -36,28 +30,6 @@ public class GoogleDriveUtils {
      * If modifying these scopes, delete your previously saved tokens/ folder.
      */
     private static final List<String> SCOPES = Collections.singletonList(DriveScopes.DRIVE);
-
-    /**
-     * Creates an authorized Credential object for standalone.
-     *
-     * @param HTTP_TRANSPORT The network HTTP Transport.
-     * @param credentials    Stream that contains a json object with user credentials info
-     * @return An authorized Credential object.
-     * @throws IOException If the credentials stream is invalid.
-     */
-    private static Credential getCredentialsStandalone(final NetHttpTransport HTTP_TRANSPORT, InputStream credentials) throws IOException {
-        // Load client secrets.
-        GoogleClientSecrets clientSecrets = GoogleClientSecrets.load(JSON_FACTORY, new InputStreamReader(credentials));
-
-        // Build flow and trigger user authorization request.
-        GoogleAuthorizationCodeFlow flow = new GoogleAuthorizationCodeFlow.Builder(
-                HTTP_TRANSPORT, JSON_FACTORY, clientSecrets, SCOPES)
-                .setDataStoreFactory(new FileDataStoreFactory(new java.io.File(TOKENS_DIRECTORY_PATH)))
-                .setAccessType("offline")
-                .build();
-        LocalServerReceiver receiver = new LocalServerReceiver.Builder().setPort(8888).build();
-        return new AuthorizationCodeInstalledApp(flow, receiver).authorize("user");
-    }
 
     /**
      * Creates an authorized Credential object for web applications, from a service account.
@@ -103,7 +75,8 @@ public class GoogleDriveUtils {
         if (Constants.CREDENTIALS_ORIGIN_STRATEGIES.SERVER.equals(credentialsStrategy)) {
             credential = getCredentialsWeb(credentials);
         } else {
-            credential = getCredentialsStandalone(HTTP_TRANSPORT, credentials);
+            throw new Exception("Not other method than SERVER is implemented");
+            // Incompatible. Google local authentication method uses javax.servlet-api-2.5 with is not allowed in modern servers
         }
         Drive service = new Drive.Builder(HTTP_TRANSPORT, JSON_FACTORY, credential)
                 .setApplicationName(APPLICATION_NAME)
